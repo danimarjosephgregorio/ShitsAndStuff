4f32677d840a8e41330c8aad6307e5f075c4883c
GREGORIO DANIMAR JOSEPH <gregorio.123279@southwoods.sti.edu.ph>
2020-03-02 10:07:47 -0800

    BinarySearchTree

---
+++ BinarySearchTree/Node.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class Node
+    {
+        public Node RightNode;
+        public Node LeftNode;
+        public int Data;
+
+        public Node(int data)
+        {
+            this.Data = data;
+        }
+
+        public void Insert(int value)
+        {
+            if(value <= Data)
+            {
+                if(LeftNode != null)
+                {
+                    LeftNode.Insert(value);
+                }
+                else
+                {
+                    LeftNode = new Node(value);
+                }
+            }
+            else
+            {
+                if (RightNode != null)
+                {
+                    RightNode.Insert(value);
+                }
+                else
+                {
+                    RightNode = new Node(value);
+                }
+            }
+        }
+
+        public void InOrderTraversal()
+        {
+            if (LeftNode != null)
+            {
+                LeftNode.InOrderTraversal();
+            }
+
+            Console.Write($"{this.Data}, ");
+
+            if (RightNode != null)
+            {
+                RightNode.InOrderTraversal();
+            } 
+        }
+
+        public void PreOrderTraversal()
+        {
+            Console.Write($"{Data}, ");
+
+            if (LeftNode != null)
+            {
+                LeftNode.PreOrderTraversal();
+            }
+
+            if (RightNode != null)
+            {
+                RightNode.PreOrderTraversal();
+            }
+        }
+
+        public void PostOrderTraversal()
+        {
+            if (LeftNode != null)
+            {
+                LeftNode.PostOrderTraversal();
+            }
+
+            if (RightNode != null)
+            {
+                RightNode.PostOrderTraversal();
+            }
+
+            Console.Write($"{Data}, ");
+        }
+
+        public bool Contains(int value)
+        {
+            if (value == Data)
+            {
+                return true;
+            }
+
+            if (value < Data)
+            {
+                if (LeftNode != null)
+                {
+                    return LeftNode.Contains(value);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (RightNode != null)
+                {
+                    return RightNode.Contains(value);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public Node FindRecursive(int value)
+        {
+            if (value == Data)
+            {
+                return this;
+            }
+
+            if (value < Data)
+            {
+                if (LeftNode != null)
+                {
+                    return LeftNode.FindRecursive(value);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                if (RightNode != null)
+                {
+                    return RightNode.FindRecursive(value);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        public Node Find(int value)
+        {
+            Node currentNode = this;
+            if (value == currentNode.Data)
+            {
+                return currentNode;
+            }
+
+            if (value < currentNode.Data)
+            {
+                if (currentNode.LeftNode != null)
+                {
+                    return currentNode.LeftNode.Find(value);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                if (currentNode.RightNode != null)
+                {
+                    return currentNode.RightNode.Find(value);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -8,8 +8,55 @@ namespace BinarySearchTree
 {
     class Program
     {
+        public static Node tree = null;
+
         static void Main(string[] args)
         {
+            InitializeTree();
+
+            Console.WriteLine("\n\nIn-order traversal: ");
+            tree.InOrderTraversal();
+
+            Console.WriteLine("\n\nPre-order traversal: ");
+            tree.PreOrderTraversal();
+
+            Console.WriteLine("\n\nPost-order traversal: ");
+            tree.PostOrderTraversal();
+
+            Console.Write($"\n\nTree contains {18}: {tree.Contains(18)}");
+
+            Console.Write($"\n\nFind node recursively: {tree.FindRecursive(99).Data}");
+
+            Console.Write($"\n\nFind node non-recursively: {tree.Find(66).Data}");
+
+            Console.ReadLine();
+        }
+
+        public static void InitializeTree()
+        {
+            tree = new Node(50);
+            tree.Insert(22);
+            tree.Insert(33);
+            tree.Insert(44);
+            tree.Insert(99);
+            tree.Insert(77);
+            tree.Insert(88);
+            tree.Insert(66);
+            tree.Insert(55);
+            tree.Insert(77);
+            tree.Insert(10);
+            tree.Insert(20);
+            tree.Insert(80);
+            tree.Insert(60);
+            tree.Insert(90);
+            tree.Insert(10);
+            tree.Insert(67);
+            tree.Insert(89);
+            tree.Insert(18);
+            tree.Insert(23);
+            tree.Insert(82);
+            tree.Insert(1);
+            tree.Insert(100);
         }
     }
 }
